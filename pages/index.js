ce2fe529a69a1ff4fb386faca6de44f6d968aa23
Zain Fiaz <106686424+zain12321@users.noreply.github.com>
2022-08-14 13:25:38 +0500

    clone

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import { useSession } from 'next-auth/react';
+import Head from 'next/head'
+import { useRouter } from 'next/router';
+import Dashboard from'../components/Dashboard'
+import Loader from '../components/Loader';
+
+export default function Home() {
+  const router = useRouter();
+  const { status, data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push("/auth/signin");
+    },
+  });
+
+  // Loading animation...
+  if (status === "loading") {
+    return <Loader />;
+  }
+
+  return (
+    <div className="">
+      <Head>
+        <title>spotify dashboard</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Dashboard />
+    </div>
+  )
+};
